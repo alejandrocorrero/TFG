e10b7a60dea43d2439c8617da914285dabc8a8c7
Alejandro correro <alejandro.correro@hotmail.com>
2018-04-24 23:51:58 +0200

    cambios api

--- app/src/main/java/com/correro/alejandro/tfg/api/ApiService.kt
+++ app/src/main/java/com/correro/alejandro/tfg/api/ApiService.kt
@@ -11,7 +11,7 @@ interface ApiService {
     @POST("oauth/v2/token")
     fun login(@Field("username") username: String, @Field("password") password: String, @Field("grant_type") type: String, @Field("client_id") clientid: String, @Field("client_secret") clientSecret: String): Observable<LoginResponse>
 
-    @GET("api/pacient/getuser")
+    @GET("api/patient/user")
     fun getUser(@Header("Authorization") token: String): Observable<UserResponse>
 
 }
